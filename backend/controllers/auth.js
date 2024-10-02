3ac1305565649580b6cba778a912c8edb2dc75ff
Joseph Eiba <josepheiba@icloud.com>
2024-10-02 20:22:23 +0100

    login v 2 for local storage instead of cookies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,8 +8,11 @@ module.exports.signin_post = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { MaxAge: maxAge * 1000, httpOnly: true });
-    res.status(200).json({ user: user._id });
+    // res.cookie("jwt", token, { MaxAge: maxAge * 1000, httpOnly: true });
+    res.status(200).json({
+      user: user._id,
+      token: token,
+    });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -20,8 +23,11 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ username, email, password });
     const token = createToken(user._id);
-    res.cookie("jwt", token, { MaxAge: maxAge * 1000, httpOnly: true });
-    res.status(201).json({ user: user._id });
+    // res.cookie("jwt", token, { MaxAge: maxAge * 1000, httpOnly: true });
+    res.status(201).json({
+      user: user._id,
+      token: token,
+    });
   } catch (error) {
     res.status(400).send(error);
   }
